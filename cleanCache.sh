a9ed9990b7bcd25ff771bce25df685e95e0bdb46
Florian VERGÉ <104826504+maitreverge@users.noreply.github.com>
2024-12-01 17:48:57 +0100

    Create cleanCache.sh

---
+++ cleanCache.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+rm -rf .var/app/com.discordapp.Discord/cache/
+rm -rf .var/app/com.google.Chrome/cache/
+rm -rf .var/app/com.slack.Slack/cache/
+rm -rf .var/app/com.visualstudio.code/cache/
+
+echo "Cache Cleaned"
