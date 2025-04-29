2f340271e8554c077f06c025cb9d5559997ae172
Florian VERGÉ <104826504+maitreverge@users.noreply.github.com>
2025-04-29 18:58:56 +0200

    Improved python_venv.sh

--- python_venv.sh
+++ python_venv.sh
@@ -9,24 +9,30 @@
 # You can find his original script at :
 # https://makwanadhruv.medium.com/automating-virtual-environments-bash-script-magic-for-python-developers-3a06df1777a6
 
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+BLUE="\e[1;34m"
+ESC="\e[0m"
+
 print_help() {
-    echo -e "Usage: $0 \e[1;33m[option]\e[0m \e[1;32m[env_name]\e[0m\n"
-	echo -e "Notes :\n1 - Your environment name \e[1;31mWILL\e[0m be named \e[1;32m.venv\e[0m if no \e[1;32m[env_name]\e[0m argument is provided."
-	echo -e "2 - Your Python Packages files \e[1;31mMUST\e[0m be named \e[1;32mrequirements.txt\e[0m or \e[1;32msetup.py\e[0m, \nand both will be installed if both are present.\n"
+    echo -e "\nUsage: $GREEN source$ESC $0 $YELLOW[option]$ESCAPE $BLUE[env_name]$ESC \n"
+	echo -e "Notes :\n1 - Your environment name$RED WILL$ESC be named $GREEN .venv$ESC if no $BLUE[env_name]$ESC argument is provided."
+	echo -e "2 - Your Python Packages files$RED MUST$ESC  be named $GREEN requirements.txt$ESC  or $GREEN setup.py$ESC , \nand both will be installed if both are present.\n"
     echo "Options:"
-	echo -e "  \e[1;33mmaster\e[0m      Create, install and activate a new virtual environment "
-    echo -e "  \e[1;33mcreate\e[0m      Create a new virtual environment"
-    echo -e "  \e[1;33mactivate\e[0m    Activate the virtual environment"
-    echo -e "  \e[1;33minstall\e[0m     Install dependencies from requirements.txt or setup.py"
-    echo -e "  \e[1;33mexport\e[0m      Export installed dependencies to requirements.txt within a virtual environment (default name: .venv)"
-    echo -e "  \e[1;33mremove\e[0m      Deactivate and remove the virtual environment"
+	echo -e "  $YELLOW master$ESC       Create, install and activate a new virtual environment "
+    echo -e "  $YELLOW create$ESC       Create a new virtual environment"
+    echo -e "  $YELLOW activate$ESC     Activate the virtual environment"
+    echo -e "  $YELLOW install$ESC      Install dependencies from requirements.txt or setup.py"
+    echo -e "  $YELLOW export$ESC       Export installed dependencies to requirements.txt within a virtual environment (default name: .venv)"
+    echo -e "  $YELLOW remove$ESC       Deactivate and remove the virtual environment"
 }
 
 check_virtualenv() {
     if ! command -v virtualenv &> /dev/null; then
-        echo "virtualenv is not installed. Installing..."
+        echo -e "$YELLOW virtualenv is not installed. Installing...$ESC"
         python3 -m pip install --user virtualenv
-        echo "virtualenv installation complete."
+        echo -e "$GREEN virtualenv installation complete.$ESC"
     fi
 }
 
@@ -38,11 +44,11 @@ create_venv() {
     local env_name=${1:-".venv"}
 
     if [ -d "$env_name" ]; then
-        echo "Virtual environment '$env_name' already exists. Aborting."
+        echo -e "$YELLOW Virtual environment '$env_name' already exists. Aborting.$ESC"
         return 1
     fi
 
-	echo -e "\n \e[1;33m--  CREATING VIRTUAL ENVIRONMENT $env_name  --\e[0m 🔧\n"
+	echo -e "\n $YELLOW --  CREATING VIRTUAL ENVIRONMENT $env_name  --$ESC  🔧\n"
 	echo "------------------------------------------"
 
     python3 -m venv "$env_name"
@@ -50,52 +56,51 @@ create_venv() {
     pip install -U pip
 
 	echo "------------------------------------------"
-	echo -e "\n \e[1;32m--  VIRTUAL ENVIRONMENT $env_name CREATED  --\e[0m ✅\n"
+	echo -e "\n $GREEN --  VIRTUAL ENVIRONMENT $env_name CREATED  --$ESC  ✅\n"
 }
 
 activate_venv() {
     local env_name=${1:-".venv"}
 
     if [ ! -d "$env_name" ]; then
-        echo "Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one."
+        echo -e "$RED Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one.$ESC"
         return 1
     fi
 
     source "./$env_name/bin/activate"
 
-	echo -e "\n \e[1;32m--  VIRTUAL ENVIRONMENT $env_name ACTIVATED  --\e[0m ✅\n"
+	echo -e "\n $GREEN --  VIRTUAL ENVIRONMENT $env_name ACTIVATED  --$ESC  ✅\n"
 }
 
 install_deps() {
 
-    
 	local env_name=${1:-".venv"}
 
     if [ ! -d "$env_name" ]; then
-        echo "Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one."
+        echo -e "$RED Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one.$ESC"
         return 1
     fi
 
     source "./$env_name/bin/activate"
 
     if [ -f "requirements.txt" ]; then
-		echo -e "\n \e[1;33m--  INSTALLING DEPENDENCIES FROM ./requirements.txt  --\e[0m 🔧\n"
+		echo -e "\n$YELLOW --  INSTALLING DEPENDENCIES FROM ./requirements.txt  --$ESC  🔧\n"
 		echo "------------------------------------------"
 
         pip install -r ./requirements.txt
 		
 		echo "------------------------------------------"
-		echo -e "\n \e[1;32m--  DEPENDENCIES INSTALLED FROM ./requirements.txt --\e[0m ✅\n"
+		echo -e "\n$GREEN --  DEPENDENCIES INSTALLED FROM ./requirements.txt --$ESC  ✅\n"
     fi
 
     if [ -f "setup.py" ]; then
-		echo -e "\n \e[1;33m--  INSTALLING DEPENDENCIES FROM ./setup.py  --\e[0m 🔧\n"
+		echo -e "\n$YELLOW --  INSTALLING DEPENDENCIES FROM ./setup.py  --$ESC  🔧\n"
 		echo "------------------------------------------"
 
         pip install -e .
 		
 		echo "------------------------------------------"
-		echo -e "\n \e[1;32m--  DEPENDENCIES INSTALLED FROM ./setup.py --\e[0m ✅\n"
+		echo -e "\n$GREEN --  DEPENDENCIES INSTALLED FROM ./setup.py --$ESC  ✅\n"
     fi
 }
 
@@ -103,17 +108,17 @@ export_deps() {
     local env_name=${1:-".venv"}
 
     if [ ! -d "$env_name" ]; then
-        echo "Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one."
+        echo -e "$RED Virtual environment '$env_name' not found. Use '$0 create [env_name]' to create one.$ESC"
         return 1
     fi
 
-	echo -e "\n \e[1;33m--  EXPORTING DEPENDENCIES TO ./requirements.txt  --\e[0m 🔧\n"
+	echo -e "\n$YELLOW --  EXPORTING DEPENDENCIES TO ./requirements.txt  --$ESC  🔧\n"
 	echo "------------------------------------------"
 
     source "./$env_name/bin/activate"
     pip freeze > requirements.txt
 	echo "------------------------------------------"
-	echo -e "\n \e[1;32m--  DEPENDENCIES EXPORTED TO ./requirements.txt  --\e[0m ✅\n"
+	echo -e "\n$GREEN --  DEPENDENCIES EXPORTED TO ./requirements.txt  --$ESC  ✅\n"
 }
 
 remove_venv() {
@@ -124,7 +129,7 @@ remove_venv() {
         return 1
     fi
 
-	echo -e "\n \e[1;33m--  REMOVING VIRTUAL ENVIRONMENT $env_name  --\e[0m 🔧\n"
+	echo -e "\n $YELLOW --  REMOVING VIRTUAL ENVIRONMENT $env_name  --$ESC  🔧\n"
 	echo "------------------------------------------"
 	# Only deactivate if there is a current active Virtual Environment
 	if [ -n "$VIRTUAL_ENV" ]; then
@@ -133,17 +138,21 @@ remove_venv() {
     rm -rf "$env_name"
 
 	echo "------------------------------------------"
-	echo -e "\n \e[1;32m--  VIRTUAL ENVIRONMENT $env_name REMOVED  --\e[0m ✅\n"
+	echo -e "\n $GREEN --  VIRTUAL ENVIRONMENT $env_name REMOVED  --$ESC  ✅\n"
 }
 
-if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+# Calls help printing
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ] ; then
     print_help
-    return 0
+    exit 1
 fi
 
+# Stops execution is the first argument is the script name
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    echo -e "\e[1;31mERROR: This script must be sourced, not executed.\e[0m"
-    echo -e "Please run: \e[1;32msource $0 [command]\e[0m or \e[1;32m. $0 [command]\e[0m"
+    echo -e "$RED ERROR: This script must be sourced, not executed.$ESC "
+    echo -e "Example:$BLUE source$ESC $GREEN$0$ESC $YELLOW master$ESC\n\n"
+    echo -e "Please run: $YELLOW$0$ESC $GREEN-h $ESC <or $GREEN--help$ESC or$GREEN help $ESC> for script usage. "
+	# print_help
     exit 1
 fi
 
@@ -169,7 +178,7 @@ case "$1" in
         remove_venv "$2"
         ;;
     *)
-        echo -e "\e[1;31mUnknown option: $1\e[0m"
+        echo -e "$RED Unknown option: $1$ESC "
         print_help
         exit 1
         ;;
