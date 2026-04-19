688ed3db124343ac7e4721b2d4ff75bd820d6045
Florian VERGÉ <104826504+maitreverge@users.noreply.github.com>
2026-04-19 21:27:59 +0200

    Delete cleanCache.sh

--- cleanCache.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-rm -rf .var/app/com.discordapp.Discord/cache/
-rm -rf .var/app/com.google.Chrome/cache/
-rm -rf .var/app/com.slack.Slack/cache/
-rm -rf .var/app/com.visualstudio.code/cache/
-
-echo "Cache Cleaned"
